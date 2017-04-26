10ef76db1dd79e73240df357476a77d4c776bce7
Judd Flamm <kd7yva@gmail.com>
2017-04-26 17:31:34 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+var processUniqueUuid = 'xxx'.replace(/[xy]/g, function(c) {
+	var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+	return v.toString(16);
+});
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var body = '<html><head><title>Sample NodeJS Webapp</title></head><body><h2>Served from Docker Container: '+processUniqueUuid+'</h2>Headers:<ul>';
+	for (var i in req.headers) {
+		body = body + ('<li>'+i+' - '+req.headers[i]+'</li>');
+	}
+	body = body + '</ul></body></html>';
+	res.end(body);
+}).listen(80);
+
+console.log('Server running at localhost:80/');
