45465960f8493a7d88b07dc1c1e4dfe78dde9ca3
Judd Flamm <juddflamm@users.noreply.github.com>
2017-04-28 11:48:07 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,28 @@ http.createServer(function (req, res) {
 		<html>
 		<head>
 		<title>Sample NodeJS Webapp</title>
+		<style>
+			html{background-color:#0c141f;min-height:100%}
+			body{margin:0}
+			body,input,select,textarea{font-family:-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Arial,sans-serif;font-size:16pt;box-sizing:border-box;color:#e6ffff;-webkit-font-smoothing:antialiased}
+			a{color:#6fc3df}
+			.monospace{font-family:Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace}
+			.hidden,.hide-until-load{visibility:hidden}
+			.container{display:block}
+			.site-heading{padding:.5em;text-align:center;margin:0;display:flex;flex-direction:column}
+			.site-heading span{display:flex;align-items:center;justify-content:center}
+			.site-heading h1{font-size:200%;margin:.3em 0 .5em .5em;letter-spacing:0;font-weight:200}
+			.site-heading img.logo{display:none}
+		</style>
 		</head>
 		<body>
-		<h2>Served from host: ${processUniqueUuid}</h2>
+		<heading class="site-heading">
+			<div class="container">
+				<span>
+				<h2>Served from host: ${processUniqueUuid}</h2>
+				</span>
+			</div>
+		</heading>
 		<hr>
 		<h4>Headers<h4>
 		<ul>
